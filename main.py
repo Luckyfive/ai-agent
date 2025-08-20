52e333f863716499ee02a00472f9d707e6e9ee56
Andres Garcia <andresf212000@gmail.com>
2025-08-19 22:09:09 -0400

    adding gen ai code

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
 def main():
     print("Hello from ai-agent!")
-
-
+    answer = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print(answer.text)
+    print(f"Prompt tokens: {answer.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {answer.usage_metadata.candidates_token_count}")
 if __name__ == "__main__":
     main()
