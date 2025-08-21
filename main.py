166b0bf294a5b2c793effdb8502f0b9ee360e3d7
Andres Garcia <andresf212000@gmail.com>
2025-08-21 16:49:56 -0400

    updating to use args

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,10 +7,16 @@ api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <input>")
+        sys.exit(1)
+
+    input_text = sys.argv[1]
+
     print("Hello from ai-agent!")
     answer = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=input_text
     )
     print(answer.text)
     print(f"Prompt tokens: {answer.usage_metadata.prompt_token_count}")
